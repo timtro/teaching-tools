7bd8caaa09a4833c84db69d762c330d2e92352fe
timtro <timtro@gmail.com>
2017-12-16 21:21:09 -0500

    Colours change when paused and when ended.

--- exam-timer/exam-timer.py
+++ exam-timer/exam-timer.py
@@ -5,12 +5,19 @@ from enum import Enum
 import datetime as dt
 from time import strftime
 
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GObject
+
+normalColur = "#FFFFFF"
+pausedColour = "#BBBBBB"
+endedColour = "#FF9090"
 
 
 class State(Enum):
     PAUSED = 0
     RUNNING = 1
+    ENDED = 2
 
 
 class ClockWindow:
@@ -18,7 +25,10 @@ class ClockWindow:
     def __init__(self, duration):
 
         self.timeRemains = duration
+        self.originalDuration = duration
         self.timeOfLastCall = dt.datetime.now()
+        self.countDownColour = "#FFFFFF"
+        self.pause()
 
         self.state = State.PAUSED
 
@@ -48,28 +58,38 @@ class ClockWindow:
         return None
 
     def update(self):
-        if self.state is State.RUNNING:
-            now = dt.datetime.now()
-            self.timeRemains -= now - self.timeOfLastCall
-            self.timeOfLastCall = now
+        if self.timeRemains <= dt.timedelta(seconds=1):
+            self.countDownColour = endedColour
+        else:
+            if self.state is State.RUNNING:
+                now = dt.datetime.now()
+                self.timeRemains -= now - self.timeOfLastCall
+                self.timeOfLastCall = now
 
         self.timetext.set_markup(
             "Current Time:\n<span size='98000'>" + strftime('%H:%M') + "</span>")
-        self.remainstext.set_markup("Time Remaining:\n<span size='98000'>" + '{0:02d}'.format(
+        self.remainstext.set_markup("Time Remaining:\n<span size='98000' color='" + self.countDownColour + "'>" + '{0:02d}'.format(
             self.timeRemains.seconds // 3600) + ':' + '{0:02d}'.format((self.timeRemains.seconds // 60) % 60) + "</span>")
         self.progressbar.set_fraction(
-            self.timeRemains.seconds / duration.seconds)
+            self.timeRemains.seconds / self.originalDuration.seconds)
 
-        # As this is a timeout function, return True so that it
-        # continues to get called
+        # Return True so that it continues to get called
         return True
 
     def toggle_pause(self):
         if self.state == State.PAUSED:
-            self.state = State.RUNNING
-            self.timeOfLastCall = dt.datetime.now()
+            self.unpause()
         else:
-            self.state = State.PAUSED
+            self.pause()
+
+    def unpause(self):
+        self.state = State.RUNNING
+        self.countDownColour = normalColur
+        self.timeOfLastCall = dt.datetime.now()
+
+    def pause(self):
+        self.state = State.PAUSED
+        self.countDownColour = pausedColour
 
     def toggle_black_and_white(self):
         if self.BW == True:
@@ -87,75 +107,6 @@ class ClockWindow:
             self.BW = True
 
 
-class EntryWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Entry Demo")
-        self.set_size_request(200, 100)
-
-        self.timeout_id = None
-
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
-
-        self.entry = Gtk.Entry()
-        self.entry.set_text("Hello World")
-        vbox.pack_start(self.entry, True, True, 0)
-
-        hbox = Gtk.Box(spacing=6)
-        vbox.pack_start(hbox, True, True, 0)
-
-        self.check_editable = Gtk.CheckButton("Editable")
-        self.check_editable.connect("toggled", self.on_editable_toggled)
-        self.check_editable.set_active(True)
-        hbox.pack_start(self.check_editable, True, True, 0)
-
-        self.check_visible = Gtk.CheckButton("Visible")
-        self.check_visible.connect("toggled", self.on_visible_toggled)
-        self.check_visible.set_active(True)
-        hbox.pack_start(self.check_visible, True, True, 0)
-
-        self.pulse = Gtk.CheckButton("Pulse")
-        self.pulse.connect("toggled", self.on_pulse_toggled)
-        self.pulse.set_active(False)
-        hbox.pack_start(self.pulse, True, True, 0)
-
-        self.icon = Gtk.CheckButton("Icon")
-        self.icon.connect("toggled", self.on_icon_toggled)
-        self.icon.set_active(False)
-        hbox.pack_start(self.icon, True, True, 0)
-
-    def on_editable_toggled(self, button):
-        value = button.get_active()
-        self.entry.set_editable(value)
-
-    def on_visible_toggled(self, button):
-        value = button.get_active()
-        self.entry.set_visibility(value)
-
-    def on_pulse_toggled(self, button):
-        if button.get_active():
-            self.entry.set_progress_pulse_step(0.2)
-            # Call self.do_pulse every 100 ms
-            self.timeout_id = GObject.timeout_add(100, self.do_pulse, None)
-        else:
-            # Don't call self.do_pulse anymore
-            GObject.source_remove(self.timeout_id)
-            self.timeout_id = None
-            self.entry.set_progress_pulse_step(0)
-
-    def do_pulse(self, user_data):
-        self.entry.progress_pulse()
-        return True
-
-    def on_icon_toggled(self, button):
-        if button.get_active():
-            stock_id = Gtk.STOCK_FIND
-        else:
-            stock_id = None
-        self.entry.set_icon_from_stock(Gtk.EntryIconPosition.PRIMARY,
-                                       stock_id)
-
-
 def on_key_press_event(widget, event):
     keyname = Gdk.keyval_name(event.keyval)
     print("Key %s (%d) was pressed" % (keyname, event.keyval))
@@ -163,9 +114,6 @@ def on_key_press_event(widget, event):
         ClockWindow.toggle_black_and_white(clockWin)
     if keyname == 'p':
         ClockWindow.toggle_pause(clockWin)
-    if keyname == 'o':
-        owin = EntryWindow()
-        owin.show_all()
 
 
 def undoBWColor(X):
@@ -182,12 +130,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # now = dt.datetime.now()
-    duration = dt.timedelta(minutes=args.mins)
+    durationFromArgs = dt.timedelta(minutes=args.mins)
 
     settings = Gtk.Settings.get_default()
     settings.set_property("gtk-application-prefer-dark-theme", True)
 
-    clockWin = ClockWindow(duration)
+    clockWin = ClockWindow(durationFromArgs)
 
     # add to the main loop scheduled tasks
     GObject.timeout_add(200, clockWin.update)
